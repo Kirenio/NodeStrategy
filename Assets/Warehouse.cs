dce1d82cdea387c03d64d7564595a00677aedd08
Kirenio <k4567.h@gmail.com>
2016-09-29 17:28:31 +0300

    Warehouse now tracks how many items have been stored Fixed Warehouse storing infinite amount of items Moved Warehouse's logging into separate function Added information about how much space is used in Warehouse to the log

--- Assets/Warehouse.cs
+++ Assets/Warehouse.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 public class Warehouse : Building {
     public new Dictionary<Resource, Cargo> Stored = new Dictionary<Resource, Cargo>();
+    float StoredAmount = 0;
     
     public override Cargo Ship(Resource type, float amount)
     {
@@ -23,20 +24,60 @@ public class Warehouse : Building {
         else return CargoCreate(type, 0);
     }
 
+    protected virtual void LogRecieved(Cargo cargo)
+    {
+        Debug.Log(string.Format("{0} {1}\nUsed {2} out of {3} space.", Stored[cargo.Type].Amount, Stored[cargo.Type].Type, StoredAmount, Capacity));
+    }
     public override Cargo Recieve(Cargo cargo)
     {
-        if (Stored.ContainsKey(cargo.Type))
+        if (StoredAmount + cargo.Amount <= Capacity)
         {
-            Stored[cargo.Type] = CargoCreate(cargo.Type, Stored[cargo.Type].Amount + cargo.Amount);
-            Debug.Log(Stored[cargo.Type].Amount + " " + Stored[cargo.Type].Type);
-            return CargoCreate(cargo.Type, 0);
+            if (Stored.ContainsKey(cargo.Type))
+            {
+                Stored[cargo.Type] = CargoCreate(cargo.Type, Stored[cargo.Type].Amount + cargo.Amount);
+                StoredAmount += cargo.Amount;
+                LogRecieved(cargo);
+                return CargoCreate(cargo.Type, 0);
+            }
+            else
+            {
+                Stored.Add(cargo.Type, cargo);
+                StoredAmount += cargo.Amount;
+                LogRecieved(cargo);
+                return CargoCreate(cargo.Type, 0);
+            }
         }
-        else if (!Stored.ContainsKey(cargo.Type))
+        else if(StoredAmount + cargo.Amount > Capacity)
         {
-            Stored.Add(cargo.Type, cargo);
-            Debug.Log(Stored[cargo.Type].Amount + " " + Stored[cargo.Type].Type);
-            return CargoCreate(cargo.Type, 0);
+            float spaceFree = Capacity - StoredAmount;
+            if(spaceFree > 0)
+            {
+                if (Stored.ContainsKey(cargo.Type))
+                {
+                    Stored[cargo.Type] = CargoCreate(cargo.Type, Stored[cargo.Type].Amount + spaceFree);
+                    StoredAmount += spaceFree;
+                    LogRecieved(cargo);
+                    return CargoCreate(cargo.Type, cargo.Amount - spaceFree);
+                }
+                else
+                {
+                    Stored.Add(cargo.Type, CargoCreate(cargo.Type, spaceFree));
+                    StoredAmount += spaceFree;
+                    LogRecieved(cargo);
+                    return CargoCreate(cargo.Type, cargo.Amount - spaceFree);
+                }
+            }
         }
-        else return cargo;
+        return cargo;
+    }
+
+    public virtual float StoredAmountRecalculate()
+    {
+        float value = 0;
+        foreach (Cargo item in Stored.Values)
+        {
+            value += item.Amount;
+        }
+        return value;
     }
 }
