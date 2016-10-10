de3910902dd63d0df30d085d7fc0d7316ed5de9d
Kirenio <k4567.h@gmail.com>
2016-10-10 05:25:54 +0300

    Added Build menu prototype Added zoom to camera Added check if pointer is above the interface Added ability to place buildings Moved LogContent debug function from right mouse button to the scroll wheel click

--- Assets/Controls.cs
+++ Assets/Controls.cs
@@ -1,17 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Controls : MonoBehaviour
 {
     public Transform CameraAnchor;
+    public int scrollMultiplier;
     public float mouseSensitivity = 0.3f;
     public float keyboardScrollSpeed = 2f;
     Building Selection;
     public Text UnitInfo;
     public SelectionPathTracking PathTracker;
+    GameObject buildingToBuild;
+    public GameObject BuildingToBuild { get { return buildingToBuild; } set { buildingToBuild = value; Debug.Log("Set build to: " + value); } }
     
     void Update()
     {
+
         if (Input.GetKey(KeyCode.W)) CameraAnchor.position += Vector3.forward * keyboardScrollSpeed * Time.deltaTime;
         if (Input.GetKey(KeyCode.S)) CameraAnchor.position += -Vector3.forward * keyboardScrollSpeed * Time.deltaTime;
         if (Input.GetKey(KeyCode.A)) CameraAnchor.position += -Vector3.right * keyboardScrollSpeed * Time.deltaTime;
@@ -24,6 +29,7 @@ public class Controls : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0))
         {
+            if (EventSystem.current.IsPointerOverGameObject()) return;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
@@ -50,8 +56,10 @@ public class Controls : MonoBehaviour
                 }
             }
         }
-        else if (Input.GetMouseButtonUp(1))
+
+        if (Input.GetMouseButtonUp(2))
         {
+            if (EventSystem.current.IsPointerOverGameObject()) return;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
@@ -60,6 +68,21 @@ public class Controls : MonoBehaviour
                 if (target != null) target.LogContent();
             }
         }
+
+        if(Input.GetMouseButtonUp(1))
+        {
+            if (EventSystem.current.IsPointerOverGameObject()) return;
+
+            if (BuildingToBuild != null)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+                {
+                    Instantiate(BuildingToBuild, hit.point, Quaternion.identity);
+                }
+            }
+        }
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize += Input.GetAxis("Mouse ScrollWheel") * scrollMultiplier,1,20);
     }
 
     void UpdateToolTip()
---
+++ Assets/ExpandableMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExpandableMenu : MonoBehaviour {
+    public Animator animator;
+    bool state = true;
+
+    public void ChangeState()
+    {
+        if (state) animator.Play("BuildingMenuExpand");
+        else animator.Play("BuildingMenuCollapse");
+        state = !state;
+    }
+}
